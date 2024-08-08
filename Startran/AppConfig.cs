47a1cbd742b0b442e3b772a4094627a6e5b7bd5d
Arborsm <BlueGrayx@qq.com>
2024-08-09 00:59:22 +0800

    and things

--- Startran/AppConfig.cs
+++ Startran/AppConfig.cs
@@ -1,6 +1,39 @@
-﻿namespace Startran;
+﻿using Newtonsoft.Json;
+
+namespace Startran;
 
 public class AppConfig
 {
-    
+    public List<string> Apis { get; } = ["OpenAI"];
+    public string Api = "OpenAI";
+    public string DirectoryPath { get; set; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "mods");
+    public string Language { get; set; } = "zh";
+    public string EnToCn { get; set; } = "你是一个游戏文本翻译助手，这次要翻译的是星露谷物语的一个模组，贴合游戏将英文意译成简体中文并加以润色使其更符合中文的语言环境，意译输入文本，不要直接使用原参考文本, 形如${'$'}1^1^[233]&${'$'}等符号组合不需要翻译务必保留在语句对应的位置，以输入格式直接输出结果。";
+
+    public OpenAiConfig OpenAi { get; set; } = new();
+
+    private static readonly string ConfigFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
+
+    public static AppConfig Load()
+    {
+        if (!File.Exists(ConfigFilePath)) return new AppConfig();
+        var json = File.ReadAllText(ConfigFilePath);
+        return JsonConvert.DeserializeObject<AppConfig>(json)!;
+    }
+
+    public void Save()
+    {
+        var json = JsonConvert.SerializeObject(this, Formatting.Indented);
+        File.WriteAllText(ConfigFilePath, json);
+    }
+
+    public class OpenAiConfig
+    {
+        public string Api { get; set; } = "your-api-key";
+        public string Url { get; set; } = "https://api.openai.com/";
+        public string Model { get; set; } = "gpt-3.5-turbo";
+        public List<string> Models { get; set; } =
+            ["gpt-3.5-turbo", "gpt-4o","gpt-4o-mini"];
+    }
 }
+
--- Startran/OpenAISettingForm.cs
+++ Startran/OpenAISettingForm.cs
@@ -1,20 +1,141 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using OpenAI;
 
 namespace Startran
 {
-    public partial class Form1 : Form
+    public partial class OpenAiSettingForm : Form
     {
-        public Form1()
+        private readonly AppConfig _config;
+        public OpenAiSettingForm(AppConfig config)
         {
+            _config = config;
+            StartPosition = FormStartPosition.CenterScreen;
             InitializeComponent();
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpenAiSettingForm));
+            modelLabel = new Label();
+            urlLabel = new Label();
+            apiLabel = new Label();
+            ModelsComboBox = new ComboBox();
+            UrlTextBox = new TextBox();
+            ApiTextBox = new TextBox();
+            SaveButton = new Button();
+            this.RefreshButton = new Button();
+            SuspendLayout();
+            // 
+            // modelLabel
+            // 
+            resources.ApplyResources(modelLabel, "modelLabel");
+            modelLabel.Name = "modelLabel";
+            // 
+            // urlLabel
+            // 
+            resources.ApplyResources(urlLabel, "urlLabel");
+            urlLabel.Name = "urlLabel";
+            // 
+            // apiLabel
+            // 
+            resources.ApplyResources(apiLabel, "apiLabel");
+            apiLabel.Name = "apiLabel";
+            // 
+            // ModelsComboBox
+            // 
+            ModelsComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            ModelsComboBox.FormattingEnabled = true;
+            resources.ApplyResources(ModelsComboBox, "ModelsComboBox");
+            ModelsComboBox.Name = "ModelsComboBox";
+            // 
+            // UrlTextBox
+            // 
+            resources.ApplyResources(UrlTextBox, "UrlTextBox");
+            UrlTextBox.Name = "UrlTextBox";
+            // 
+            // ApiTextBox
+            // 
+            resources.ApplyResources(ApiTextBox, "ApiTextBox");
+            ApiTextBox.Name = "ApiTextBox";
+            // 
+            // SaveButton
+            // 
+            resources.ApplyResources(SaveButton, "SaveButton");
+            SaveButton.Name = "SaveButton";
+            SaveButton.UseVisualStyleBackColor = true;
+            SaveButton.Click += SaveButton_Click;
+            // 
+            // RefreshButton
+            // 
+            resources.ApplyResources(this.RefreshButton, "RefreshButton");
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += this.RefreshButton_Click;
+            // 
+            // OpenAiSettingForm
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(this.RefreshButton);
+            Controls.Add(SaveButton);
+            Controls.Add(modelLabel);
+            Controls.Add(urlLabel);
+            Controls.Add(apiLabel);
+            Controls.Add(ModelsComboBox);
+            Controls.Add(UrlTextBox);
+            Controls.Add(ApiTextBox);
+            Name = "OpenAiSettingForm";
+            Load += OpenAISettingForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private void SaveButton_Click(object? sender, EventArgs e)
+        {
+            Save();
+            Close();
+        }
+
+        private void Save()
+        {
+            _config.OpenAi.Api = ApiTextBox.Text;
+            _config.OpenAi.Url = UrlTextBox.Text;
+            _config.OpenAi.Model = ModelsComboBox.SelectedItem!.ToString()!;
+        }
+
+        private void OpenAISettingForm_Load(object? sender, EventArgs e)
+        {
+            ApiTextBox.Text = _config.OpenAi.Api;
+            UrlTextBox.Text = _config.OpenAi.Url;
+            ModelsComboBox.Items.AddRange([.. _config.OpenAi.Models]);
+            ModelsComboBox.SelectedItem = _config.OpenAi.Model;
+        }
+
+        private async void RefreshButton_Click(object? sender, EventArgs e)
+        {
+            Save();
+            try
+            {
+                using var httpClient = new HttpClient();
+                var api = new OpenAIAuthentication(_config.OpenAi.Api);
+                var url = new OpenAIClientSettings(_config.OpenAi.Url);
+                var client = new OpenAIClient(api, url, httpClient);
+                var models = await client.ModelsEndpoint.GetModelsAsync();
+                var list = models.Select(it => it.ToString()).ToList();
+                ModelsComboBox.Items.Clear();
+                ModelsComboBox.Items.AddRange([.. list]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
